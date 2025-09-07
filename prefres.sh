41086c968d31ee4faa080587a143925f8a65c063
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-07 11:22:50 +0300

    minor changes

--- prefres.sh
+++ prefres.sh
@@ -12,10 +12,10 @@ then
   xrandr --output eDP-1-1 --off
 fi
 case $selection in
-  1)xrandr --output HDMI-0 --mode 1920x1080 --right-of eDP-1-1  -r 75 --rotate right
-    xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal;;
-  2)xrandr --output HDMI-0 --mode 1920x1080 --right-of eDP-1-1 -r 75 --rotate normal
-    xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal;;	  
+  1)xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal
+    xrandr --output HDMI-0 --mode 1920x1080 --right-of eDP-1-1  -r 75 --rotate right;;
+  2)xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal
+    xrandr --output HDMI-0 --mode 1920x1080 --right-of eDP-1-1 -r 75 --rotate normal;;
   3)xrandr --output eDP-1-1 --mode 1920x1080 -r 144 --rotate normal
     xrandr --output HDMI-0 --mode 1920x1080 -r 75 --rotate normal;;
   4)xrandr --output eDP-1-1 --mode 1920x1080 -r 144 --rotate normal;;
