b1929933b20a0c6097e845180e3c483a086bf5ba
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-07 11:22:51 +0300

    Primary
    always selects first monitor as a primary, if it's available

--- prefres.sh
+++ prefres.sh
@@ -4,7 +4,7 @@ echo "   2- Join- Linear_Extended"
 echo "   3- Mirror"
 echo "   4- First Monitor Only"
 echo "   5- Second Monitor Only"
-echo "   6- Second Monitor Only-Vertical"
+echo "   6- Seconf Monitor Only-Vertical"
 read selection
 if [[ $selection > 0 ]] && [[ $selection < 7 ]];
 then
@@ -13,13 +13,13 @@ then
   xrandr --output eDP-1-1 --off
 fi
 case $selection in
-  1)xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal
+  1)xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal --primary
     xrandr --output HDMI-0 --mode 1920x1080 --right-of eDP-1-1  -r 75 --rotate right;;
-  2)xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal
+  2)xrandr --output eDP-1-1 --mode 1920x1080 --left-of HDMI-0 -r 144 --rotate normal --primary
     xrandr --output HDMI-0 --mode 1920x1080 --right-of eDP-1-1 -r 75 --rotate normal;;
-  3)xrandr --output eDP-1-1 --mode 1920x1080 -r 144 --rotate normal
+  3)xrandr --output eDP-1-1 --mode 1920x1080 -r 144 --rotate normal --primary
     xrandr --output HDMI-0 --mode 1920x1080 -r 75 --rotate normal;;
-  4)xrandr --output eDP-1-1 --mode 1920x1080 -r 144 --rotate normal;;
+  4)xrandr --output eDP-1-1 --mode 1920x1080 -r 144 --rotate normal --primary;;
   5)xrandr --output HDMI-0 --mode 1920x1080 -r 75 --rotate normal;;
   6)xrandr --output HDMI-0 --mode 1920x1080 -r 75 --rotate right;;
   *)echo "------Invalid Selection";;
