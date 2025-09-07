90457bc29c7f9105e8c5e739a4e3df89ff079e32
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-07 11:22:51 +0300

    Update fedoraEasy.sh

--- fedoraEasy.sh
+++ fedoraEasy.sh
@@ -14,9 +14,9 @@ function rpmFusion
 function additionalRepo
 	{
 		echo -e '\n-->Additional Repo Selected\n'
-		$yes | sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+		sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 		sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-		$yes | sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
+		sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
 		sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
 		sudo dnf check-update
 	}
